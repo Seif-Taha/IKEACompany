b8da7f424547b8a076c1ff01629f26db3ee853a8
Seif <saif419713@fci.bu.edu.eg>
2024-09-19 20:46:59 +0200

    Part 04 MVC Project Structure

---
+++ LinkDev.IKEACompany.PL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LinkDev.IKEACompany.PL.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
